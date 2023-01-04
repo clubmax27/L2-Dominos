4b947f21dc1aac89f1fa13ce6bf267bed6920b1e
Melodiah <121955557+Melodiah@users.noreply.github.com>
2023-01-04 20:25:53 +0100

    Update Jeu.java
    Ajout main , sac , tuile , initiation du plateau et début des tours des joueurs , rajout du constructeur de jeu pour faciliter l'utilisation de fonction

--- Jeu.java
+++ Jeu.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Jeu {
 
-	public void partie() {
+	Plateau p ;
+	Sac s ;
+	ArrayList<Joueur> ListJoueur ;
+	
+	public Jeu(Plateau p , Sac s , ArrayList<Joueur> j) {
+		this.p = p ;
+		this.s = s ;
+		this.ListJoueur = j ;
+	}
+	
+	public static void partie() {
 		Scanner scanner = new Scanner(System.in) ;
 		
 		ArrayList<Joueur> a = new ArrayList<Joueur>() ;
@@ -13,18 +23,54 @@ public class Jeu {
 		int nbJoueur = ScanNbJoueur() ;
 
 		// Tant que le nombre de joueur est incorrect
-		while(nbJoueur > 8 && nbJoueur < 2) {
-			System.out.println("Le nombre de Joueur doit être inférieur ou égal à huit. Veuillez changez le nombre");
+		while(nbJoueur > 8 || nbJoueur < 2) {
+			System.out.println("Le nombre de Joueur doit être inférieur ou égal à huit et supérieur ou égal à deux. Veuillez changez le nombre");
 			nbJoueur = ScanNbJoueur() ;
 		}
 
 		// On ajoute tout les joueurs à la liste des joueurs 
-		for(int i = 0 ; i <= nbJoueur ;i++ ) {
-			a.add(ScanNomJoueur(i)) ;
+		for(int i = 0 ; i < nbJoueur ;i++ ) {
+			a.add(ScanNomJoueur(i+1)) ;
+		}
+	
+		// On demande le nombre de de tuiles à utiliser 
+		int nbSac = ScanNbTuile() ;
+		// On cré ensuite le sac 
+		Sac s = new Sac(nbSac) ;
+		
+		
+		// Création du plateau de jeu 
+		Plateau Plateaujeu = new Plateau() ;
+		Plateaujeu.plateau.get(0).add(s.CreaTuile()) ;
+		
+		Jeu part = new Jeu(Plateaujeu , s ,a) ;
+		
+		while(FinJeu(a,s) == false) {
+			for(int i = 0 ; i < a.size() ; i++) {
+				System.out.println("Tour de " + a.get(i).nom);
+				part.Tour(i);
+			}
 		}
 	}
 	
-	public int ScanNbJoueur() {
+	public void Tour(int n) {
+		
+		if(this.ListJoueur.get(n).abandon != true) {
+			this.ListJoueur.get(n).main = this.s.CreaTuile() ;
+		}
+	}
+	
+	public static int ScanNbTuile() {
+		Scanner scanner = new Scanner(System.in) ;
+		
+		System.out.println("Entrez le nombre de Tuile que vous voulez dans le sac : ");
+		
+		int num = scanner.nextInt() ;
+		return num ;  
+	}
+	
+	
+	public static int ScanNbJoueur() {
 		Scanner scanner = new Scanner(System.in) ;
 		
 		System.out.println("Entrez le nombre de joueur :");
@@ -33,7 +79,7 @@ public class Jeu {
 		return num ;  
 	}
 	
-	public Joueur ScanNomJoueur(int i) {
+	public static Joueur ScanNomJoueur(int i) {
 		Scanner scanner = new Scanner(System.in) ;
 		
 		System.out.println("Entrez le nom du Joueur " + i );
@@ -41,4 +87,25 @@ public class Jeu {
 		String s = scanner.next() ;
 		return (new Joueur(s)) ;
 	}
+	
+	public static boolean FinJeu(ArrayList<Joueur> J , Sac s) {
+		if(s.Restant <= 0) {
+			return true ;
+		}
+		int abandon = 0 ; 
+		for(int i = 0 ; i< J.size() ; i++ ) {
+			if(J.get(i).abandon) {
+				abandon ++ ;
+			}
+		}
+		if(abandon >= J.size()-1 ) {
+			return true ;
+		}
+		return false ;
+	}
+	
+	public static void main(String[] args) {
+		partie() ;
+	}
+	
 }
